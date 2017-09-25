179ca82a4012cdb37af769079a384fe048075108
Curdarious Bell <BellCurdarious@yahoo.com>
2017-09-24 19:20:21 -0500

    added stylesheet

---
+++ index.js
@@ -0,0 +1,12 @@
+var age = prompt("What's your age?")
+
+if (age < 18) {
+    console.log("You cannot enter you wee man!!!");
+    }
+else if (age >= 18 && age < 21)  {
+    console.log("You can enter but you cannot drink!");
+}
+
+else {
+    console.log("Come on in and enjoy!");
+}
